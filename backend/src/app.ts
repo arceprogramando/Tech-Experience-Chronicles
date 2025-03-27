8210f5e24fec970c0a4995cb70e90ceb2db7b16b
Felipe Arce <arceprogramando@gmail.com>
2025-03-27 01:19:48 -0300

    feat: add CORS support and fetch data from backend in frontend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,25 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+  })
+);
 
 app.use(express.json());
 
+const data = {
+  name: 'John Doe',
+  age: 30,
+  address: '123 Main St',
+};
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello from Vercel!');
+  res.json(data);
 });
 
 export default app;
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,36 @@
-import { JSX } from "react";
+'use client';
+
+import { JSX, useEffect, useState } from 'react';
+
+// Definir la interfaz para los datos
+interface ApiResponse {
+  name: string;
+}
 
 export default function Home(): JSX.Element {
+  const [data, setData] = useState<ApiResponse | null>(null); // Especificamos que data será de tipo ApiResponse o null
+  const [error, setError] = useState<boolean>(false);
+
+  useEffect((): void => {
+    fetch('http://localhost:3000')
+      .then((response): Promise<ApiResponse> => {
+        // Indicamos que la respuesta será un objeto de tipo ApiResponse
+        if (!response.ok) {
+          throw new Error('Failed to fetch');
+        }
+        return response.json();
+      })
+      .then((data: ApiResponse): void => setData(data))
+      .catch((): void => setError(true));
+  }, []);
+
+  if (error) {
+    return <div>Failed to fetch</div>;
+  }
+
   return (
     <>
-      <h3>HomePage</h3> 
+      <h3>{data ? data.name : 'Loading...'}</h3>
     </>
   );
 }
