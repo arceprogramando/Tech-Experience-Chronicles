56f72d97b16baad1157e7aa9d0fa7d9b02477ed0
Felipe Arce <arceprogramando@gmail.com>
2025-04-06 23:54:22 -0300

    test: test endpoints connections

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,35 +2,52 @@
 
 import { JSX, useEffect, useState } from 'react';
 
-// Definir la interfaz para los datos
-interface ApiResponse {
-  name: string;
+interface User {
+  _id: string;
+  username: string;
+  email: string;
 }
 
 export default function Home(): JSX.Element {
-  const [data, setData] = useState<ApiResponse | null>(null); // Especificamos que data será de tipo ApiResponse o null
+  const [data, setData] = useState<User[] | null>(null);
   const [error, setError] = useState<boolean>(false);
+  const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
+  console.log('API_URL', API_URL);
   useEffect((): void => {
-    fetch('http://localhost:3000')
-      .then((response): Promise<ApiResponse> => {
-        // Indicamos que la respuesta será un objeto de tipo ApiResponse
+    fetch(`${API_URL}/user/getAll`)
+      .then((response): Promise<User[]> => {
         if (!response.ok) {
           throw new Error('Failed to fetch');
         }
-        return response.json();
+        return response.json(); // Esto es una promesa de User[]
       })
-      .then((data: ApiResponse): void => setData(data))
+      .then((users: User[]): void => setData(users))
       .catch((): void => setError(true));
-  }, []);
+  }, [API_URL]);
 
   if (error) {
-    return <div>Failed to fetch</div>;
+    return <div>❌ Error al obtener los usuarios {error}</div>;
+  }
+
+  if (!data) {
+    return <div>⏳ Cargando...</div>;
   }
 
   return (
-    <>
-      <h3>{data ? data.name : 'Loading...'}</h3>
-    </>
+    <div>
+      <h2>Usuarios</h2>
+      <ul>
+        {data.map(
+          (user: User): JSX.Element => (
+            <li key={user._id}>
+              <p>User: {user.username} </p>
+
+              <p>Email: {user.email ? user.email : 'No email provided'}</p>
+            </li>
+          )
+        )}
+      </ul>
+    </div>
   );
 }
