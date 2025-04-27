d7b8b037b59cc0ddac533571836890b40b64bf74
Felipe Arce <arceprogramando@gmail.com>
2025-04-27 18:58:08 -0300

    feat: add ExplorePage component and update Header to use ReactElement

---
+++ frontend/src/app/(pages)/explore/page.tsx
@@ -0,0 +1,10 @@
+import { ReactElement } from 'react';
+
+export default function ExplorePage(): ReactElement {
+  return (
+    <div className="container flex flex-col items-center justify-center gap-4 py-8 text-black">
+      <h1 className="text-3xl font-bold">Explorar</h1>
+      <p className="text-lg">Explora las diferentes pruebas tecnicas</p>
+    </div>
+  );
+}
--- frontend/src/features/ui/header.tsx
+++ frontend/src/features/ui/header.tsx
@@ -1,9 +1,20 @@
-import { JSX } from 'react';
+import { ReactElement } from 'react';
 import NavigationMenu from './navigation/navigationmenu';
+import Link from 'next/link';
 
-export default function Header(): JSX.Element {
+export default function Header(): ReactElement {
   return (
-    <header className="container mx-auto gap-4 py-2 text-white ">
+    <header className="container flex items-center justify-between gap-4 py-2 text-black">
+      <div className="flex items-center gap-8">
+        <h1 className="font-bold text-2xl">
+          <Link href="/">
+            <span className="text-violet-500">Tech </span>Tests
+          </Link>
+        </h1>
+        <Link href="/explore" className="text-base font-medium">
+          Explorar
+        </Link>
+      </div>
       <NavigationMenu />
     </header>
   );
