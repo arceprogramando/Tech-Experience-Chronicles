7c4058a6f9de6e5375d20e2349ecbb0154e907da
Felipe Arce <arceprogramando@gmail.com>
2025-04-27 18:58:25 -0300

    feat: update linksData to include HomePage link and adjust import style

--- frontend/src/features/ui/navigation/linksdata.ts
+++ frontend/src/features/ui/navigation/linksdata.ts
@@ -1,7 +1,6 @@
-import { Links } from "./types/links";
+import { Links } from './types/links';
 
 const linksData: Links[] = [
-  { url: '/', title: 'HomePage' },
   { url: '/login', title: 'Login' },
   { url: '/register', title: 'Sign Up' },
 ];
