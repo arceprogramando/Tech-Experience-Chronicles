f4d01f33331396bab16c6e9de1324337c3f25024
Felipe Arce <arceprogramando@gmail.com>
2025-04-27 18:57:46 -0300

    feat: refactor Home component to remove user fetching logic and update layout

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,53 +1,12 @@
-'use client';
+import { ReactElement } from 'react';
 
-import { JSX, useEffect, useState } from 'react';
-
-interface User {
-  _id: string;
-  username: string;
-  email: string;
-}
-
-export default function Home(): JSX.Element {
-  const [data, setData] = useState<User[] | null>(null);
-  const [error, setError] = useState<boolean>(false);
-  const API_URL = process.env.NEXT_PUBLIC_API_URL;
-
-  console.log('API_URL', API_URL);
-  useEffect((): void => {
-    fetch(`${API_URL}/user/getAll`)
-      .then((response): Promise<User[]> => {
-        if (!response.ok) {
-          throw new Error('Failed to fetch');
-        }
-        return response.json(); // Esto es una promesa de User[]
-      })
-      .then((users: User[]): void => setData(users))
-      .catch((): void => setError(true));
-  }, [API_URL]);
-
-  if (error) {
-    return <div>❌ Error al obtener los usuarios {error}</div>;
-  }
-
-  if (!data) {
-    return <div>⏳ Cargando...</div>;
-  }
-
+export default function Home(): ReactElement {
   return (
-    <div>
-      <h2>Usuarios</h2>
-      <ul>
-        {data.map(
-          (user: User): JSX.Element => (
-            <li key={user._id}>
-              <p>User: {user.username} </p>
-
-              <p>Email: {user.email ? user.email : 'No email provided'}</p>
-            </li>
-          )
-        )}
-      </ul>
+    <div className="flex flex-col items-center">
+      <h2 className="text-4xl font-bold">Plataforma de Pruebas Técnicas</h2>
+      <p className="text-gray-500">
+        Descubre, comparte y aprende de pruebas técnicas reales
+      </p>
     </div>
   );
 }
